249d366ee2fe5c2452b7ad56341fc89f9adf22d3
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-02-08 13:24:06 +0000

    Alan Partridge Kata

--- src/main/scala/Partridge.scala
+++ src/main/scala/Partridge.scala
@@ -4,8 +4,17 @@ object Partridge {
   // See the README file for explanations of what each function should do.
 
   def part(args: Array[String]): String = {
-    ???
-  }
+    val terms = Array("Partridge", "PearTree", "Chat", "Dan", "Toblerone", "Lynn", "AlphaPapa", "Nomad")
+
+    val x = args.filter(x => terms.contains(x)).length
+
+      if (x == 0) {
+        "Lynn, I've pierced my foot on a spike!!"
+      } else {
+        s"Mine's a Pint${"!" * x}"
+      }
+    }
+
 
   def apple(x: Any): String = {
     ???
