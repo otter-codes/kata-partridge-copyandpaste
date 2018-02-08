5bdec2639d299aaf11dca7311a9ef73850992b73
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-02-08 15:55:17 +0000

    added apples boolean test

--- src/main/scala/Partridge.scala
+++ src/main/scala/Partridge.scala
@@ -1,13 +1,9 @@
 object Partridge {
-
   // IMPORTANT: Do not change the signatures of the functions declared below.
   // See the README file for explanations of what each function should do.
-
   def part(args: Array[String]): String = {
     val terms = Array("Partridge", "PearTree", "Chat", "Dan", "Toblerone", "Lynn", "AlphaPapa", "Nomad")
-
-    val x = args.filter(x => terms.contains(x)).length
-
+    val x = args.count(x => terms.contains(x))
       if (x == 0) {
         "Lynn, I've pierced my foot on a spike!!"
       } else {
@@ -15,14 +11,13 @@ object Partridge {
       }
     }
 
-
   def apple(x: Any): String = {
     val n = x match {
       case i: Int => i
       case s: String => s.toInt
       case _ => throw new IllegalArgumentException("Nah bruv, gotta be an Int or String")
-    }
-    if (n * n > 1000) {
+        }
+      if (n * n > 1000) {
       "It's hotter than the sun!!"
     } else {
       "Help yourself to a honeycomb Yorkie for the glovebox"
@@ -30,11 +25,8 @@ object Partridge {
   }
 
   def london(args: Array[String]): String = {
-
     val stations = Array("Rejection", "Disappointment", "Backstabbing Central", "Shattered Dreams Parkway")
-
     val x = args.filter(x => stations.contains(x))
-
     if (x sameElements stations ) {
       "Smell my cheese you mother!"
     } else {
